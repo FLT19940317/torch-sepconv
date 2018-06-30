790dfa4fc0f2f2b29896ada1387e67110723e5cb
sniklaus <simon.niklaus@outlook.com>
2018-06-29 23:00:49 -0700

    no message

--- install.bash
+++ install.bash
@@ -2,5 +2,5 @@
 
 luarocks make rocks/nnex-scm-1.rockspec
 
-wget --timestamping http://content.sniklaus.com/SepConv/network-l1.t7
-wget --timestamping http://content.sniklaus.com/SepConv/network-lf.t7
+wget --timestamping http://content.sniklaus.com/sepconv/network-l1.t7
+wget --timestamping http://content.sniklaus.com/sepconv/network-lf.t7
